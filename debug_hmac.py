705a4533c8bdec2b26fecd23d43ae704b1cea829
yennguyen <zillgioloc@gmail.com>
2026-05-29 19:59:38 +0700

    chore: untrack debug_hmac.py and test_llm.py

--- debug_hmac.py
+++
@@ -1,9 +0,0 @@
-import hmac, hashlib
-from src.core.config import settings
-
-body = '{"message": "test"}'
-ts = '1779095898'
-sig = hmac.new(settings.HMAC_SECRET.encode(), f'{ts}{body}'.encode(), hashlib.sha256).hexdigest()
-print('HMAC_SECRET:', repr(settings.HMAC_SECRET))
-print('Input     :', repr(f'{ts}{body}'))
-print('Signature :', sig)
